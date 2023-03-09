997221798f59324fc61be51fb68ddd01314ccc0d
dg1223 <alavi1223@hotmail.com>
2023-03-09 18:44:33 -0500

    shopping cart skeleton UI

---
+++ js/cart.js
@@ -0,0 +1,16 @@
+const addProduct = () => {
+  const productField = document.getElementById("product-name");
+  const quantityField = document.getElementById("product-quantity");
+  const product = productField.value;
+  const quantity = quantityField.value;
+  productField.value = "";
+  quantityField.value = "";
+  displayProduct(product, quantity);
+};
+
+const displayProduct = (product, quantity) => {
+  const ul = document.getElementById("product-container");
+  const li = document.createElement("li");
+  li.innerText = `${product}: ${quantity}`;
+  ul.appendChild(li);
+};
